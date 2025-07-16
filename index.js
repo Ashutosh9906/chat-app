f41ba95ea24bdda05df2d3325b1d5e985c57a658
Ashutosh9906 <ashutoshgandule1@gmail.com>
2025-07-16 22:30:56 +0530

    created database

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io")
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+async function main() {
+    //open databse files
+    const db = await open({
+        filename: "chat.db",
+        driver: sqlite3.Database,
+    })
+
+    //create message table
+    await db.exec(`
+        CREATE TABLE IF NOT EXIST MESSAGE(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+        )
+    `)
+}
 
 const app = express()
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
 
 app.get("/", (rq, res) => {
     res.sendFile(join(__dirname, "views", "index.html"))
@@ -16,8 +37,16 @@ io.on("connection", (socket) => {
     // socket.on("disconnect", () => {
     //     console.log("User disconnected");
     // })
-    socket.on("user message", (msg) => {
-        io.emit("message", msg);
+    socket.on("user message", async (msg) => {
+        let result;
+        try {
+            //store the message in databse
+            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (e) {
+            //To handle the error
+            return;
+        }
+        io.emit("message", msg, result/lastID);
     })
 });
 
